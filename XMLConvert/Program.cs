8d30c4f4a44d8416e6a9c99d24e069c3c62baa8b
Evan <evanz1215@gmail.com>
2024-08-01 19:37:22 +0800

    加入專案檔案。

---
+++ XMLConvert/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+var xmlString = File.ReadAllText("PurchaseOrder.xml");
+
+// 1. 傳統方法: 轉成PurchaseOrder物件 (推薦)
+
+var serializer = new XmlSerializer(typeof(PurchaseOrder));
+using var reader = new StringReader(xmlString);
+var purchaseOrder = (PurchaseOrder)serializer.Deserialize(reader)!;
+
+Console.WriteLine("information from xml:");
+Console.WriteLine("part no:");
+
+foreach (var item in purchaseOrder.Items)
+{
+    Console.WriteLine(item.PartNumber);
+}
+Console.WriteLine();
+
+// 2. 傳統方法: 使用 XmlDocument, 操作xml node(節點), 非物件操作 (不推薦)
+// https://learn.microsoft.com/zh-tw/dotnet/api/system.xml.xmlnode.selectnodes?view=net-8.0
+Console.WriteLine("using XmlDocument");
+
+var doc = new XmlDocument();
+doc.LoadXml(xmlString);
+
+// https://learn.microsoft.com/zh-tw/dotnet/api/system.xml.xmlnode.selectnodes?view=net-8.0#system-xml-xmlnode-selectnodes(system-string)
+// https://learn.microsoft.com/zh-tw/previous-versions/dotnet/netframework-4.0/ms256086(v=vs.100)
+var partNumbers = doc.SelectNodes("//@PartNumber");
+
+Console.WriteLine("part numbers in doc:");
+foreach (XmlNode node in partNumbers)
+{
+    Console.WriteLine(node.InnerText);
+}
+Console.WriteLine();
+
+// 3. 使用LINQ方法: 直接使用LINQ操作xml字串取得並要資訊 (推薦)
+Console.WriteLine("using LINQ:");
+
+var xml = XElement.Parse(xmlString);
+var partNos = xml.Descendants("Item").Select(x => (string)x.Attribute("PartNumber")).ToList();
+
+foreach (var partNo in partNos)
+{
+    Console.WriteLine(partNo);
+}
+Console.WriteLine();
+
+
+var query = from item in xml.Descendants("Item")
+            let partNo = (string)item.Attribute("PartNumber")
+            //let productName = (string)item.Element("ProductName")
+            let quantity = (int)item.Element("Quantity")
+            let unitPrice = (double)item.Element("USPrice")
+            select new { partNo, quantity, unitPrice };
+var list = query.ToList();
+
+list.ForEach(x =>
+{
+    Console.WriteLine($"{x.partNo}, {x.quantity}, {x.unitPrice}");
+});
+Console.WriteLine();
---
+++ XMLConvert/PurchaseOrder.cs
@@ -0,0 +1,320 @@
+﻿
+// 注意: 產生的程式碼可能至少需要 .NET Framework 4.5 或 .NET Core/Standard 2.0。
+/// <remarks/>
+[System.SerializableAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+[System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+public partial class PurchaseOrder
+{
+
+    private PurchaseOrderAddress[] addressField;
+
+    private string deliveryNotesField;
+
+    private PurchaseOrderItem[] itemsField;
+
+    private uint purchaseOrderNumberField;
+
+    private System.DateTime orderDateField;
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlElementAttribute("Address")]
+    public PurchaseOrderAddress[] Address
+    {
+        get
+        {
+            return this.addressField;
+        }
+        set
+        {
+            this.addressField = value;
+        }
+    }
+
+    /// <remarks/>
+    public string DeliveryNotes
+    {
+        get
+        {
+            return this.deliveryNotesField;
+        }
+        set
+        {
+            this.deliveryNotesField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlArrayItemAttribute("Item", IsNullable = false)]
+    public PurchaseOrderItem[] Items
+    {
+        get
+        {
+            return this.itemsField;
+        }
+        set
+        {
+            this.itemsField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public uint PurchaseOrderNumber
+    {
+        get
+        {
+            return this.purchaseOrderNumberField;
+        }
+        set
+        {
+            this.purchaseOrderNumberField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute(DataType = "date")]
+    public System.DateTime OrderDate
+    {
+        get
+        {
+            return this.orderDateField;
+        }
+        set
+        {
+            this.orderDateField = value;
+        }
+    }
+}
+
+/// <remarks/>
+[System.SerializableAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+public partial class PurchaseOrderAddress
+{
+
+    private string nameField;
+
+    private string streetField;
+
+    private string cityField;
+
+    private string stateField;
+
+    private uint zipField;
+
+    private string countryField;
+
+    private string typeField;
+
+    /// <remarks/>
+    public string Name
+    {
+        get
+        {
+            return this.nameField;
+        }
+        set
+        {
+            this.nameField = value;
+        }
+    }
+
+    /// <remarks/>
+    public string Street
+    {
+        get
+        {
+            return this.streetField;
+        }
+        set
+        {
+            this.streetField = value;
+        }
+    }
+
+    /// <remarks/>
+    public string City
+    {
+        get
+        {
+            return this.cityField;
+        }
+        set
+        {
+            this.cityField = value;
+        }
+    }
+
+    /// <remarks/>
+    public string State
+    {
+        get
+        {
+            return this.stateField;
+        }
+        set
+        {
+            this.stateField = value;
+        }
+    }
+
+    /// <remarks/>
+    public uint Zip
+    {
+        get
+        {
+            return this.zipField;
+        }
+        set
+        {
+            this.zipField = value;
+        }
+    }
+
+    /// <remarks/>
+    public string Country
+    {
+        get
+        {
+            return this.countryField;
+        }
+        set
+        {
+            this.countryField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string Type
+    {
+        get
+        {
+            return this.typeField;
+        }
+        set
+        {
+            this.typeField = value;
+        }
+    }
+}
+
+/// <remarks/>
+[System.SerializableAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+public partial class PurchaseOrderItem
+{
+
+    private string productNameField;
+
+    private byte quantityField;
+
+    private decimal uSPriceField;
+
+    private System.DateTime shipDateField;
+
+    private bool shipDateFieldSpecified;
+
+    private string commentField;
+
+    private string partNumberField;
+
+    /// <remarks/>
+    public string ProductName
+    {
+        get
+        {
+            return this.productNameField;
+        }
+        set
+        {
+            this.productNameField = value;
+        }
+    }
+
+    /// <remarks/>
+    public byte Quantity
+    {
+        get
+        {
+            return this.quantityField;
+        }
+        set
+        {
+            this.quantityField = value;
+        }
+    }
+
+    /// <remarks/>
+    public decimal USPrice
+    {
+        get
+        {
+            return this.uSPriceField;
+        }
+        set
+        {
+            this.uSPriceField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlElementAttribute(DataType = "date")]
+    public System.DateTime ShipDate
+    {
+        get
+        {
+            return this.shipDateField;
+        }
+        set
+        {
+            this.shipDateField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlIgnoreAttribute()]
+    public bool ShipDateSpecified
+    {
+        get
+        {
+            return this.shipDateFieldSpecified;
+        }
+        set
+        {
+            this.shipDateFieldSpecified = value;
+        }
+    }
+
+    /// <remarks/>
+    public string Comment
+    {
+        get
+        {
+            return this.commentField;
+        }
+        set
+        {
+            this.commentField = value;
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlAttributeAttribute()]
+    public string PartNumber
+    {
+        get
+        {
+            return this.partNumberField;
+        }
+        set
+        {
+            this.partNumberField = value;
+        }
+    }
+}
+
